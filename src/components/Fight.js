a7c6bfa3487f5a73f2a03d2096661e6622fc9354
eunseo <k.eunseo122@gmail.com>
2022-08-09 16:22:31 +0200

    fight

--- src/components/Fight.js
+++ src/components/Fight.js
@@ -5,12 +5,18 @@ import './fight.css';
 const Fight = ({allData}) => {
   const {id} = useParams();
 
+  const [playerState, setPlayerState] = useState(0)
+  const [computerState, setComputerState] = useState(0)
+
   if(!allData[id-1]) return <p>Loading</p>;
 
   const {base, name} = allData[id-1];
+  if(base.HP != playerState) setPlayerState(base.HP);
+
   const computerRand = Math.floor(Math.random()*allData.length);
   const computerPokemon = allData[computerRand];
-  
+  if(computerPokemon.base.HP === 0) setComputerState(computerPokemon.base.HP)
+    
   return (
     <div className="fight-container-box">
     <div className="fight-container">
