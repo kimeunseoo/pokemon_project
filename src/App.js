c66cc94dd85853a1ccbb3b98b2a8ed15c795b6d1
Fennec92 <nazar.hamidulla@posteo.de>
2022-08-10 18:13:02 +0200

    fight fixed

--- src/App.js
+++ src/App.js
@@ -10,9 +10,26 @@ import Fight from "./components/Fight"
 function App() {
   const[pokeApi, setPokeApi]= useState([]);
   const url ='https://pokefight-g03-api.herokuapp.com/pokemon';
+  const [computerPokemon, setComputerPokemon] = useState({
+    "HP": 0,
+    "Attack": 0,
+    "Defense": 0,
+    "Sp. Attack":0,
+    "Sp. Defense":0,
+    "Speed": 0
+  })
+
+  const getComputerPokemon = (res) => {
+    const computerRand = Math.floor(Math.random()*res.length);
+    if(res.length>0){
+      const addComputerPokemon = res[computerRand];
+      setComputerPokemon(addComputerPokemon.base)
+      console.log("hier" + computerPokemon)
+    }
+  }
 
   const getFetch = () => {
-    fetch(url)
+    return fetch(url)
       .then((res) => {
         // console.log(res);
         // console.log(data);
@@ -20,24 +37,30 @@ function App() {
       })
       .then((res) => {
         setPokeApi(res)
+        return res;
       });
   };
+
   useEffect(() => {
     try {
-      getFetch();
+      getFetch().then((res)=>getComputerPokemon(res));
     } catch (error) {
       console.log("Error: " + error);
     }
-  }, []);
 
-  console.log(pokeApi)
+  }, []);
+  
+
+  
+  // console.log(pokeApi)
+  console.log(computerPokemon)
 
 
 
   return (
 <div>
    <Routes>
-    <Route path="/pokemon/:id/fight" element={<Fight allData={pokeApi} />} />    
+    <Route path="/pokemon/:id/fight" element={<Fight allData={pokeApi} pokemon={computerPokemon} setPokemon={setComputerPokemon}/>}/>    
     <Route path="/pokemon/:id" element={<DetailPokemon allData={pokeApi} />} />
     <Route path="/pokemon" element={<AllPokemon allData={pokeApi} />} />   
     <Route path="/" element={<Home />} />   
--- src/components/Fight.js
+++ src/components/Fight.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useParams } from "react-router-dom";
 import './fight.css';
 
-const Fight = ({allData}) => {
+const Fight = ({allData, pokemon, setPokemon}) => {
   const {id} = useParams();
 
   const [playerState, setPlayerState] = useState({
@@ -14,16 +14,20 @@ const Fight = ({allData}) => {
     "Speed": 0
   })
 
-  const [computerState, setComputerState] = useState({
-      "HP": 0,
-      "Attack": 0,
-      "Defense": 0,
-      "Sp. Attack":0,
-      "Sp. Defense":0,
-      "Speed": 0
-    })
-
-  console.log(playerState);
+  
+  console.log("fight rendered")
+  
+  // const [computerState, setComputerState] = useState({
+  //     "HP": 0,
+  //     "Attack": 0,
+  //     "Defense": 0,
+  //     "Sp. Attack":0,
+  //     "Sp. Defense":0,
+  //     "Speed": 0
+  //   })
+  
+  const [playerHP, setPlayerHP] = useState(0)
+  const [computerHP, setComputerHP] =useState(0)
 
   if(!allData[id-1]) return <p>Loading</p>;
   
@@ -36,48 +40,64 @@ const Fight = ({allData}) => {
     base["Sp. Attack"] !== playerState["Sp. Attack"] &&
     base["Sp. Defense"] !== playerState["Sp. Defense"] &&
     base.Speed !== playerState.Speed 
-    ) setPlayerState(base);
-  
-  const computerRand = Math.floor(Math.random()*allData.length);
-  const computerPokemon = allData[computerRand];
+    ) {
+      setPlayerState(base);
+      setPlayerHP(base.HP);
+      setComputerHP(pokemon.HP);
+    }
+    
 
-  if(
-    computerPokemon.base.HP !== computerState.HP &&
-    computerPokemon.base.Attack !== computerState.Attack &&
-    computerPokemon.base.Defense !== computerState.Defense &&
-    computerPokemon.base["Sp. Attack"] !== computerState["Sp. Attack"] &&
-    computerPokemon.base["Sp. Defense"] !== computerState["Sp. Defense"] &&
-    computerPokemon.base.Speed !== computerState.Speed
-    ) setComputerState(computerPokemon.base)
+  if(base.Speed < pokemon.Speed){
+    setPlayerHP(prev => prev-5)
+    setPokemon(prev => {
+      return {
+        ...prev,
+        Speed: 0
+      }
+    })
+  }
+
+
+
+  
+  const playerFight = () => {
+    setComputerHP(prev => prev -5);
+  }
+  const computerFight = () => {
+    setPlayerHP(prev => prev -5);
+  }
+  
 
   
   
-
   return (
     <div className="fight-container-box">
     <div className="fight-container">
       <div className="computer">
         <div>
-          <h1>{computerPokemon.name.english}</h1>
-          <h3>HP: {computerState.HP}</h3>
-          <h3>ATK: {computerState.Attack}</h3>
-          <h3>DEF: {computerState.Defense}</h3>
-          <h3>SPD: {computerState.Speed}</h3>
+          {/* <h1>{pokemon.name.english}</h1> */}
+          <h3>HP: {computerHP}</h3>
+          <h3>ATK: {pokemon.Attack}</h3>
+          <h3>DEF: {pokemon.Defense}</h3>
+          <h3>SPD: {pokemon.Speed}</h3>
         </div>
         <div className="button-container">
-          <button className="pokemon-attack">Attack</button>
+          <button onClick={computerFight} className="pokemon-attack">Attack</button>
         </div>
       </div>
       <div className="player">
         <div>
           <h1>{name.english}</h1>
-          <h3>HP: {playerState.HP}</h3>
+          <h3>HP: {playerHP}</h3>
           <h3>ATK: {playerState.Attack}</h3>
           <h3>DEF: {playerState.Defense}</h3>
           <h3>SPD: {playerState.Speed}</h3>
         </div>
         <div className="button-container">
-          <button className="pokemon-attack">Attack</button>
+          <button 
+            onClick={playerFight}
+            className="pokemon-attack"
+          >Attack</button>
         </div>
       </div>
     </div>
